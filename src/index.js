756694d19b1e436baf6c4f70571e721f321306b0
rsodst <rsodst@gmail.com>
2020-02-19 22:54:04 +0500

    feat: implemented reverse method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (String(n).charAt(0) == '-') {
+        return parseInt(String(n).substr(1).split('').reverse().join(''));
+    } else {
+        return parseInt(String(n).split('').reverse().join(''));
+    }
 }
